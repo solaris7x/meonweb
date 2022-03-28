dea8ba6f07401eb7dfc44238b05e54b4deead4d8
solaris7x <question211199@gmail.com>
2022-03-29 00:09:00 +0530

    Added Google Tag script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css"
 // import "../styles/style.css"
 import type { AppProps } from "next/app"
 import Head from "next/head"
+import Script from "next/script"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,19 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', "${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}");
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   )
